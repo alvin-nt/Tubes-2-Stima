52b5eb4639aeea20ca31a9c574e3b5c77216c4f5
dalva24 <dariel24@gmail.com>
2014-03-28 08:04:35 +0700

    fixed some stuff missing and added SQL query filtering

---
+++ WebCrawler/WebCrawler/webui/crawl.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <header>
+	<title>Stima Tubes 2 Search Engine</title>
+	<link href="style.css" type="text/css" rel="stylesheet"></link>
+    </header>
+    <body>
+	<div class = "maincontainer">
+	    <br />
+	    <h1>HST: Hubble Search Telescope</h1>
+	    <p>"Because binoculars for search icons are too mainstream"</p>
+	    <br />
+	    <p>
+		<b>This is a random Page Describing the Crawler, and a link to it</b><br/><br/>
+		BLABLALBalbalsglajgnahvrotur snhvsu ludshnfvgskdhf gsui <br/>
+		sadlvnhas oghnnhg porhnsdvf hgldjfgh skf hglsdfvjnsdf<br/>
+		na slghn ldjfh pgoch fgsdfh ;ld nvdcnvk ldjvnlkjnvlk<br/>
+		langsdfnhgpso dfhpvnsfnh ldjfsh nvdsjnfvldhf<br/>
+		asmjdgisdnhf dshfngnvsd ;vnihvakljhnvlkd jfbvldnj<br/>
+	    </p>
+	</div>
+	<div class = "linkscontainer">
+	    <div class = "link"><a href="index.php">Back to Search</a></div>
+	</div>
+    </body>
+</html>
